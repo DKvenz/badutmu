384bf70c5f144f280e5d9fbad7b9bfd3a9d77ad3
Dhika Ardiansyah <88916859+DKvenz@users.noreply.github.com>
2021-11-17 21:02:11 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $host ="localhost"; //host server
-  $user ="root"; //user login phpMyAdmin
-  $pass =""; //pass login phpMyAdmin
-  $db ="badutmu"; //nama database
-  $conn = mysqli_connect($host, $user, $pass, $db) or die ("Koneksi gagal");
-?>
